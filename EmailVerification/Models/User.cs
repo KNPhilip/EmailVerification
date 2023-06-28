840fb612b737aab9aae79eb06d220d31131c75d3
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-28 20:51:09 +0200

    Add project files.

---
+++ EmailVerification/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailVerification.Models
+{
+    public class User
+    {
+        public required int Id { get; set; }
+        public string? Email { get; set; } = string.Empty;
+        public string? PasswordHash { get; set; } = string.Empty;
+        public string? VerificationToken { get; set; } = string.Empty;
+        public DateTime? VerifiedAt { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+    }
+}
