2bd46708289b53b6b5843d4fefa0ff0895ca9bd9
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-28 21:32:42 +0200

    Created the UserLoginRequest model.

---
+++ EmailVerification/Models/UserLoginRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailVerification.Models
+{
+    public class UserLoginRequest
+    {
+        [Required, EmailAddress]
+        public required string Email { get; set; } = string.Empty;
+        [Required]
+        public required string Password { get; set; } = string.Empty;
+    }
+}
--- EmailVerification/Models/UserRegisterRequest.cs
+++ EmailVerification/Models/UserRegisterRequest.cs
@@ -4,7 +4,7 @@
     {
         [Required, EmailAddress]
         public required string Email { get; set; } = string.Empty;
-        [Required, MinLength(6)]
+        [Required, MinLength(6, ErrorMessage = "The password needs to be at least 6 characters.")]
         public required string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
         public required string ConfirmPassword { get; set; } = string.Empty;
